f3b8ef37ea65cfd4776b9bf1fa3f173ba135eac7
Ryan Wilcox <rwilcox@wilcoxd.com>
2023-11-04 16:56:07 -0400

    style

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,6 @@ enum Commands {
 fn main() {
     let cli = Cli::parse();
 
-
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
