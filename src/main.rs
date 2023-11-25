496fb8266f52a231fab2d489c77fd506ffbbc022
Ryan Wilcox <rwilcox@wilcoxd.com>
2023-11-25 10:31:40 -0500

    a proper(ish) iterator for taskfiles

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #[macro_use] extern crate prettytable;
 
-use crate::taskfile::taskfile::Taskfile;
+use crate::taskfile::taskfile::{Taskfile, TaskDefinition};
 use clap::{Parser, Subcommand};
 use std::path::PathBuf;
-use prettytable::{Table, Row, Cell};
+use prettytable::{Table};
+
 
 pub mod taskfile;
 
@@ -21,12 +22,41 @@ struct Cli {
     taskfile: Option<PathBuf>,
 }
 
+
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// lists all availiable tasks
     List,
     Run
 }
+
+
+/// for the given Vector< ... of Taskfiles> loop through the tasks in each one.
+/// Tasks directly inside (each item) will be iterated over first, then external
+/// taskfile declarations inside those taskfiles will be iterated over in the same manner.
+///
+/// # Arguments
+/// * `taskfiles` - Taskfiles to iterate over
+/// * `callback_closure` - A mutable closure that takes a reference to a `TaskDefinition` object, called for every task this iterator encounters. If the closure returns false the iteration is STOPPED
+fn taskfile_iterator<F>(taskfiles: &Vec<Box<Taskfile>>,
+                     mut callback_closure: F) where F: FnMut(&TaskDefinition) -> bool {
+
+    for task_list in taskfiles {
+        for x in task_list.tasks.iter() {
+            let res = callback_closure(x);
+            if !res {
+                return
+            }
+        }
+    }
+
+    let externals: Vec<Box<Taskfile>> = taskfiles.iter().flat_map( |x| { x.externals.clone()}).collect();
+
+    if externals.len() > 0 {
+        taskfile_iterator(&externals, callback_closure);
+    }
+}
+
 
 fn main() {
     let cli = Cli::parse();
@@ -40,20 +70,14 @@ fn main() {
 
             // NOTE: this styling does not work in eshell, but works everywhere else...
             table.add_row(row![b => "name", "description"]);
-            // todo: refactor this to use the standard iterator... which will certainly handle more than 1 deep external...
-            for x in task_list.tasks.iter() {
+
+            let b = Box::new(task_list);
+            taskfile_iterator(&vec![b], |x| -> bool {
                 let task_name = x.name.clone().unwrap_or("UNKNOWN".to_string());
                 let description = x.description.clone().unwrap_or(" ".to_string());
-
                 table.add_row(row![task_name, description]);
-            }
-            for external in task_list.externals {
-                for x in external.tasks.iter() {
-                    let task_name = x.name.clone().unwrap_or("UNKNOWN".to_string());
-                    let description = x.description.clone().unwrap_or(" ".to_string());
-                    table.add_row(row![task_name, description]);
-                }
-            }
+                true
+            });
 
             table.printstd();
         }
